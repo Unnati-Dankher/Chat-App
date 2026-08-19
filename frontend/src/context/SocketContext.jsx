4671ba56111ebe2af28d7a744d2186db5ff033dd
Unnati-Dankher <unnatiknl999@gmail.com>
2026-08-19 15:58:05 +0530

    Backend Base URLs changed in frontend repo

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -25,7 +25,7 @@ export const SocketProvider = ({ children }) => {
         }
 
         // User logged in
-        const newSocket = io("http://localhost:5000", {
+        const newSocket = io(import.meta.env.VITE_SOCKET_URL, {
             query: {
                 userId: authUser._id,
             },
